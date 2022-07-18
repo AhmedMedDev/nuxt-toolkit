2cf00849b7089bce29c7db9ecac84418c0ca50d3
unknown <dedoa82.as@gmail.com>
2022-07-18 21:26:22 +0200

    Get Route Param

--- pages/index.vue
+++ pages/index.vue
@@ -1,55 +1,81 @@
 <template>
-  <v-simple-table class="pa-6">
-    <template v-slot:default>
-      <thead>
-        <tr>
-          <th class="text-left">
-            title
-          </th>
-          <th class="text-left">
-            completed
-          </th>
-          <th class="text-left">
-            Actions
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="item in todos" :key="item.id">
-          <td>{{ item.title }}</td>
-          <td>
-            <span v-if="item.completed" class="rounded-lg success white--text pa-1">completed</span>
-            <span v-else class="rounded-lg error white--text pa-1">uncompleted</span>
-          </td>
-          <td>
-            <v-btn @click="remove(item.id)" color="error ml-3" elevation="2">Delete</v-btn>
-          </td>
-        </tr>
-      </tbody>
-    </template>
-  </v-simple-table>
+  <div>
+    <v-simple-table class="pa-6">
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="text-left">
+              title
+            </th>
+            <th class="text-left">
+              completed
+            </th>
+            <th class="text-left">
+              Actions
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in todos" :key="item.id">
+            <td>
+              <nuxt-link :to="`todo/${item.id}`">{{ item.title }}</nuxt-link>
+            </td>
+            <td>
+              <span v-if="item.completed" class="rounded-lg success white--text pa-1">completed</span>
+              <span v-else class="rounded-lg error white--text pa-1">uncompleted</span>
+            </td>
+            <td>
+              <v-btn @click="remove(item.id)" color="error ml-3" elevation="2">Delete</v-btn>
+            </td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+    <v-snackbar v-model="snackbar" :color="type">
+      {{ text }}
+      <template v-slot:action="{ attrs }">
+        <v-btn color="white" text v-bind="attrs" @click="snackbar = false">
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
+  </div>
 </template>
 
 <script>
 import axios from 'axios'
 export default {
-  async created() {
+  created() {
 
-    let response = await axios.get('https://jsonplaceholder.typicode.com/todos?_page=1&_limit=5');
-
-    this.todos = response.data
+    axios.get('https://jsonplaceholder.typicode.com/todos', {
+      params: {
+        _page: 1,
+        _limit: 5,
+      }
+    })
+      .then(response => this.todos = response.data)
+      .catch(err => console.log(err))
   },
   methods: {
-    async remove (id) {
-      let response = await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
-
-      console.log(response);
+    remove(id) {
+      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        .then(response => this.notify('Task Completed Successfully', 'green'))
+        .catch(err => this.notify(`Oops : ${err}`, 'error'))
+    },
+    notify(message, type) {
+      this.snackbar = true;
+      this.text = message;
+      this.type = type;
     }
   },
   data() {
     return {
-      completed : false,
+      completed: false,
       todos: [],
+      snackbar: false,
+      text: '',
+      type: '',
+      timeout: 2000,
     }
   },
 }
---
+++ pages/todo/_id.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        Title : {{ todo.title }} <br>
+        Completed : {{ todo.completed }}
+    </div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        let id = this.$route.params.id;
+
+        this.$axios.$get(`https://jsonplaceholder.typicode.com/todos/${id}`)
+            .then(res => this.todo = res)
+            .catch(err => console.log(err))
+    },
+    data() {
+        return {
+            todo: {
+                title: 'Loading ...',
+                completed: 'Loading ...',
+            }
+
+        }
+    },
+}
+</script>
