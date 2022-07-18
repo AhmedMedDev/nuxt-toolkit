232c99f109a396530e244f1105a376126992c17f
unknown <dedoa82.as@gmail.com>
2022-07-18 04:42:32 +0200

    post table

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,52 @@
 <template>
-  <div>
-    Hello From Home
-  </div>
+  <v-simple-table class="pa-6">
+    <template v-slot:default>
+      <thead>
+        <tr>
+          <th class="text-left">
+            Name
+          </th>
+          <th class="text-left">
+            Calories
+          </th>
+          <th>
+            Actions
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in desserts" :key="item.name">
+          <td>{{ item.name }}</td>
+          <td>{{ item.calories }}</td>
+          <td>
+            <v-btn color="primary" elevation="2">Edit</v-btn>
+            <v-btn color="error ml-3" elevation="2">Delete</v-btn>
+          </td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  data() {
+    return {
+      desserts: [
+        {
+          name: 'Frozen Yogurt',
+          calories: 159,
+        },
+        {
+          name: 'Ice cream sandwich',
+          calories: 237,
+        },
+        {
+          name: 'Eclair',
+          calories: 262,
+        }
+      ],
+    }
+  },
 }
 </script>
