6da32040fc0f0f6eb25f55a448fbb98eef8e5105
unknown <dedoa82.as@gmail.com>
2022-07-21 18:11:05 +0200

    Vuex :: post store configs

--- store/index.js
+++ store/index.js
@@ -3,15 +3,26 @@ export const state = () => ({
 })
 
 export const mutations = {
+    SET_POST(state, posts) {
+        state.posts = posts
+    },
     ADD_POST(state, post) {
+        state.posts.push(post)
+    },
+    REMOVE_POST(state, post) {
         state.posts.push(post)
     }
 }
 
 export const actions = {
     async fetchPosts(state) {
+
         const response = await this.$axios.$get('posts')
-        state.posts = response.payload.data
-        return response.payload.data;
+        state.commit('SET_POST', response.payload.data)
+    },
+    async deletePost(state, id) {
+        
+        const response = await this.$axios.$delete(`posts/${id}`)
+        state.commit('SET_POST', response.payload.data)
     }
 }
